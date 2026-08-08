6741f4ce6f0db5d1401dd356ab9918ca941c8e85
HARSH RANJAN <justtheself@gmail.com>
2026-08-08 10:45:25 +0530

    Update build-retreats.js

--- scripts/build-retreats.js
+++ scripts/build-retreats.js
@@ -35,7 +35,7 @@ const END_MARKER = '<!-- CMS:RETREATS:END -->';
 
 // SET THIS to your existing Apps Script Web App URL (same one
 // booking.html/contact.html already post to).
-const APPS_SCRIPT_ENDPOINT = 'REPLACE_WITH_YOUR_APPS_SCRIPT_WEB_APP_URL';
+const APPS_SCRIPT_ENDPOINT = 'https://script.google.com/macros/s/AKfycbyBRC53kbCsrsU-22Jx3C7Vci2tMlFjpMi4_XXNmnARwl9Um5_-peKFpPnLj2-foFty/exec';
 
 function escapeHtml(str = '') {
   return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
